15aa64a11ca0f992fb2f0965afe253306d1b6d9d
cmontellanob <carlosmontellano@gmail.com>
2026-02-26 17:28:52 -0400

    26-02-26

---
+++ 02-26 Soquets TCP/server-hola-tcp/src/main/java/com/sis258/server/hola/tcp/ServerHolaTcp.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.sis258.server.hola.tcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Dell
+ */
+public class ServerHolaTcp {
+
+    public static void main(String[] args) {
+        int port = 5002;
+        ServerSocket server;
+        try {
+            // TODO code application logic here
+            server = new ServerSocket(port);
+            System.out.println("Se inicio el servidor con éxito");
+            Socket client;
+            PrintStream toClient;
+            client = server.accept(); //conexion entre cliente y servidor para comunicacion bidireccional
+            BufferedReader fromClient = new BufferedReader(new InputStreamReader(client.getInputStream())); // el lector
+            System.out.println("Cliente se conecto");
+            String recibido = fromClient.readLine();
+            System.out.println("El cliente envio el mensaje:" + recibido);
+            toClient = new PrintStream(client.getOutputStream());
+            toClient.println("Hola Mundo desde el Servidor");
+
+        } catch (IOException ex) {
+            System.out.print(ex.getMessage());
+        }
+    }
+}
