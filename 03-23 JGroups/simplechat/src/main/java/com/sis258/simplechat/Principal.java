419cb5b74a70eaa21918304adbeaef0ac27af80f
cmontellanob <carlosmontellano@gmail.com>
2026-03-30 14:38:13 -0400

    23-03-26

--- 03-23 JGroups/simplechat/src/main/java/com/sis258/simplechat/Principal.java
+++ 03-23 JGroups/simplechat/src/main/java/com/sis258/simplechat/Principal.java
@@ -14,10 +14,12 @@ public class Principal {
 
     public static void main(String[] args) {
         try {
+            
             Scanner sc = new Scanner(System.in);
             System.out.print("Introduzca su nombre");
             String nombre;
             nombre = sc.next();
+            System.setProperty("java.net.preferIPv4Stack", "true");
             new SimpleChat(nombre).start();
         } catch (Exception ex) {
             System.getLogger(Principal.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
